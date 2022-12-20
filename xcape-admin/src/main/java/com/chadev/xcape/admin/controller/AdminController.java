bb213980c3e123762b8dc0d63c200abdb0a4a778
hanseongseo <seeman94@naver.com>
2022-12-20 18:57:42 +0900

    getMerchantsByAdmin -> AdminController 로 이동 API 설명 주석 작성

--- xcape-admin/src/main/java/com/chadev/xcape/admin/controller/AdminController.java
+++ xcape-admin/src/main/java/com/chadev/xcape/admin/controller/AdminController.java
@@ -3,19 +3,41 @@ package com.chadev.xcape.admin.controller;
 import controller.request.ThemeCreateRequest;
 import controller.request.ThemeModifyRequest;
 import controller.response.ErrorCode;
+import controller.response.MerchantResponse;
 import controller.response.Response;
 import domain.dto.ThemeDto;
+import domain.entity.Admin;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import service.MerchantService;
 import service.ThemeService;
 
+import java.util.List;
+
+/**
+ * GET "/merchants" -> admin 이 갖고 있는 모든 지점 정보 가져오기
+ * POST "/merchants/{merchantId}/themes" -> merchant 에 theme 등록하기
+ * PUT "/themes/{themeId}" -> theme 수정
+ * DELETE "/themes/{themeId}" -> theme 삭제
+ */
 @Slf4j
 @RequiredArgsConstructor
 @RestController
 public class AdminController {
 
     private final ThemeService themeService;
+    private final MerchantService merchantService;
+
+    @GetMapping("/merchants")
+    public Response<List<MerchantResponse>> getMerchantsByAdmin(Admin admin) {
+        try {
+            List<MerchantResponse> merchantResponseList = merchantService.getMerchantsByAdmin(admin).stream().map(MerchantResponse::fromDto).toList();
+            return Response.success(merchantResponseList);
+        } catch (Exception e) {
+            return Response.error(ErrorCode.INVALID_PERMISSION);
+        }
+    }
 
     @PostMapping("/merchants/{merchantId}/themes")
     public Response<Void> createThemeByMerchantId(@PathVariable Long merchantId, @RequestBody ThemeCreateRequest request) {
--- xcape-core/src/main/java/controller/CoreController.java
+++ xcape-core/src/main/java/controller/CoreController.java
@@ -3,41 +3,45 @@ package controller;
 import controller.response.ErrorCode;
 import controller.response.MerchantResponse;
 import controller.response.Response;
-import domain.entity.Admin;
+import controller.response.ThemeResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import service.CoreService;
+import service.MerchantService;
+import service.ThemeService;
 
 import java.util.List;
+
+/**
+ * 공통 API
+ * GET "/merchants/{merchantId}" -> 한 지점 정보 가져오기
+ * GET "/themes/{themeId}" -> 한 테마 정보 가져오기
+ */
 
 @RequiredArgsConstructor
 @RestController
 public class CoreController {
 
-    private final CoreService coreService;
+    private final MerchantService merchantService;
+    private final ThemeService themeService;
 
     @GetMapping("/merchants/{merchantId}")
-    public Response<MerchantResponse> getMerchant(Admin admin, @PathVariable Long merchantId) {
-        MerchantResponse response = MerchantResponse.fromDto(coreService.getMerchant(merchantId));
+    public Response<MerchantResponse> getMerchant(@PathVariable Long merchantId) {
+        MerchantResponse response = MerchantResponse.fromDto(merchantService.getMerchant(merchantId));
         return Response.success(response);
     }
 
-    @GetMapping("/merchants")
-    public Response<List<MerchantResponse>> getMerchants(Admin admin) {
-        try {
-            List<MerchantResponse> merchantResponseList = coreService.getMerchantsByAdmin(admin).stream().map(MerchantResponse::fromDto).toList();
-            return Response.success(merchantResponseList);
-        } catch (Exception e) {
-            return Response.error(ErrorCode.INVALID_PERMISSION);
-        }
+    @GetMapping("/themes/{themeId}")
+    public Response<ThemeResponse> getThemeByThemeId(@PathVariable Long themeId) {
+        return Response.success(ThemeResponse.fromDto(themeService.getThemeByThemeId(themeId)));
     }
 
+    //  테스트용
     @GetMapping("/all-merchants")
     public Response<List<MerchantResponse>> getAllMerchants() {
         try {
-            List<MerchantResponse> merchantResponseList = coreService.getAllMerchants().stream().map(MerchantResponse::fromDto).toList();
+            List<MerchantResponse> merchantResponseList = merchantService.getAllMerchants().stream().map(MerchantResponse::fromDto).toList();
             return Response.success(merchantResponseList);
         } catch (Exception e) {
             return Response.error(ErrorCode.INVALID_PERMISSION);
