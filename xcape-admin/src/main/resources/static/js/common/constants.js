0a461c3393d1be6ea99ab256b3134e79052944a9
kojoo112 <kapella000@gmail.com>
2023-04-16 09:15:23 +0900

    constants수정

--- xcape-admin/src/main/resources/static/js/common/constants.js
+++ xcape-admin/src/main/resources/static/js/common/constants.js
@@ -1,5 +1,3 @@
-const GENERAL_PRICE_AREA = 'generalPriceArea';
-const OPEN_ROOM_PRICE_AREA = 'openRoomPriceArea';
 const SUCCESS = 'SUCCESS';
 const GENERAL = 'general';
 const OPEN_ROOM = 'openRoom';
