1572dc8db7533bcd64c2a936b5798fa22b2637e9
Ryu Kyungbin <kyungbin.ryu@linecorp.com>
2023-05-07 22:54:53 +0900

    add-time-zone-to-api

--- xcape-api/src/main/java/com/chadev/xcape/api/ApiApplication.java
+++ xcape-api/src/main/java/com/chadev/xcape/api/ApiApplication.java
@@ -1,11 +1,14 @@
 package com.chadev.xcape.api;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.TimeZone;
 
 @EnableJpaAuditing
 @EnableEncryptableProperties
@@ -15,5 +18,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        // timezone 설정
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 }
