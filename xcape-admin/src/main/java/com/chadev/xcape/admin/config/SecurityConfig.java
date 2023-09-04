2b477185255c5350a14d2d5ed713370065c280f6
mum <mumum3521@naver.com>
2023-09-04 19:15:54 +0900

    Add command

--- xcape-admin/src/main/java/com/chadev/xcape/admin/config/SecurityConfig.java
+++ xcape-admin/src/main/java/com/chadev/xcape/admin/config/SecurityConfig.java
@@ -17,6 +17,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         return http
                 .rememberMe()
                 .alwaysRemember(false)
