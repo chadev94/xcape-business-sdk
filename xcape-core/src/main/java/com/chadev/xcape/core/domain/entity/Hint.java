b78df0c12bb89f158d1d11b392fb7791ff41cbc4
mum <mumum3521@naver.com>
2023-10-07 00:59:44 +0900

    hint 엔티티 생성

---
+++ xcape-core/src/main/java/com/chadev/xcape/core/domain/entity/Hint.java
@@ -0,0 +1,43 @@
+package com.chadev.xcape.core.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "hint")
+public class Hint extends AuditingFields {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "hint_id")
+    private Long id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "message1")
+    private String message1;
+
+    @Column(name = "message2")
+    private String message2;
+
+    @Column(name = "is_used")
+    private boolean isUsed;
+
+    @Column(name = "registered_by")
+    private String registeredBy;
+
+    @Column(name = "modifㅑed_by")
+    private String modifiedBy;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "theme_id")
+    private Theme theme;
+
+}
+
